6df0d483b854d5eb70c787a2860106e5d0b0083e
nata100401 <41800099+nata100401@users.noreply.github.com>
2018-11-09 15:28:27 -1100

    Update and rename Desarrollo/ js/Ejercicios.js to  Desarrollo/ js/Ejercicios.js

--- Desarrollo/ js/Ejercicios.js
+++ Desarrollo/ js/Ejercicios.js
@@ -1,27 +1,4 @@
-// // //**1 EJERCICIO: INTERES BANCARIO */
-// var valorPresente=parseInt(prompt("Ingrese en NUMEROS la cantidad de dinero que desea depositar"));
-// alert("El interés reconocido mensualmente es del 2% del monto total depositado");
-//  document.write("<h1>El valor depositado es:  </h1>"+valorPresente)
-// var high=valorPresente*Math.pow(1+0.02,60);
-// document.write("<h2>Interes en 5 años</h2>"+high)
-
-
-
-
-//**2 EJERCICIO:FRUTERIA */
-var kilo;
-var kilos=4200;
-
-var kilo=prompt("Ingrese el NUMERO  de kilos adquiridos",kilo);
-
-console.log("Los kilos a comprar son: ",kilo);
-if(kilo <=2){
-    descuento=kilo*kilos*0/100;
-}
-if(kilo >2,1 && kilo <=5,0){
-    descuento=kilo*kilos*10/100;
-
-        //**1 EJERCICIO: INTERES BANCARIO */
+//**1 EJERCICIO: INTERES BANCARIO */
 var name;
 var dinner;
 dinner=prompt("Digite su deposito");
